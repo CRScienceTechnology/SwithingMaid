7f8b44e51ed790d919aa601bbc9fef0c01a9747d
ESJIAN <3340580252@qq.com>
2025-03-16 17:04:33 +0800

    feat: 添加 ESP01S-SwithingMaid.ino 文件
    - 实现了基于 ESP8266 的 WiFi 连接和 MQTT 通信
    - 添加了 PWM 控制功能，可根据 MQTT 消息控制 PWM 信号
    - 实现了 JSON 消息解析和处理逻辑
    - 包含了重新连接 WiFi 和 MQTT 服务器的逻辑

---
+++ ESP01S-SwithingMaid.ino
@@ -0,0 +1,203 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h> // 引入
+#include <ArduinoJson.h> // 引入ArduinoJson库,解析JSON用
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+// GPIO Pin Configuration
+#define PWM_Pin 0 // corresonding GPIO 0 as the PWM_PIN
+
+// Maid Property
+String maidcode="01"; //swithing maid code number
+
+// WiFi
+const char *ssid = "04"; // Enter your WiFi name
+const char *password = "12345678";  // Enter WiFi password
+
+// MQTT Server
+const char *mqtt_broker = "47.94.167.240"; //MQTT服务器的IPV4地址，不需要加http前缀
+const int mqtt_port = 1883;                //MQTT服务器的IPV4地址开放端口
+
+
+// MQTT User
+// 用于改写的String类
+String mqtt_username_str = "swithingmaid" + maidcode;
+String mqtt_password_str = "swithingmaid" + maidcode;
+String topic_str = "swithing_maid_" + maidcode + "_state";
+// 实际传入函数的字符串指针变量
+const char *mqtt_username = mqtt_username_str.c_str();  //调用.c_str()方法把字符串变为字符串指针
+const char *mqtt_password = mqtt_password_str.c_str();
+const char *topic = topic_str.c_str();
+
+
+
+                                  
+
+long int currentMillis=0;
+long int period_ms=0;
+long int Task_curentMillis=0; 
+bool ledState =false;
+
+
+void callback(char *topic, byte *payload, unsigned int length);         // 回调函数，用于Loop消息函数的回调处理
+
+
+
+void setup() {  
+    //setup阶段做一些前提准备
+    Serial.begin(115200); // Set software serial baud to 115200;
+    delay(1000);          // Delay for stability
+
+    // Connecting to a WiFi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to the WiFi network");
+
+    // Connecting to an MQTT broker
+    client.setServer(mqtt_broker, mqtt_port);  // 设置IPV4地址及端口并尝试连接目标MQTT Server
+    while (!client.connected()) 
+    {
+        String client_id = "ESP01S";
+        client_id += String(WiFi.macAddress());
+        Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str()); //client_id.c_str()
+
+        if (client.connect("ESP01S", mqtt_username, mqtt_password)) {
+            Serial.println("Public EMQX MQTT Server connected");
+            
+            client.publish(topic, "swithing maid is ready"); //发布主题
+            client.subscribe(topic);             //订阅主题
+            client.setCallback(callback);        //设置对订阅主题新消息的回调处理函数
+
+            // 打印调试信息
+            Serial.print("MQTT Username: ");
+            Serial.println(mqtt_username);
+            Serial.print("MQTT Password: ");
+            Serial.println(mqtt_password);
+            Serial.print("MQTT Topic: ");
+            Serial.println(topic);
+                
+        } else {
+            Serial.print("Failed with state ");
+            Serial.print(client.state());
+            delay(2000);
+        }
+    }
+
+}
+
+
+void loop()
+{
+    // 检查WiFi连接状态
+    if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("WiFi connection lost. Reconnecting...");
+        WiFi.begin(ssid, password);
+        while (WiFi.status() != WL_CONNECTED) {
+            delay(500);
+            Serial.println("Connecting to WiFi...");
+        }
+        Serial.println("Connected to the WiFi network");
+
+        // 重新连接MQTT broker
+        while (!client.connected()) 
+        {
+            String client_id = "ESP01S";
+            client_id += String(WiFi.macAddress());
+            Serial.printf("The client %s connects to the public MQTT broker\n", client_id.c_str());
+
+            if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+                Serial.println("Public EMQX MQTT Server connected");
+                
+                client.publish(topic, "swithing maid is ready"); //发布主题
+                client.subscribe(topic);             //订阅主题
+                client.setCallback(callback);        //设置对订阅主题新消息的回调处理函数
+
+                // 打印调试信息
+                Serial.print("MQTT Username: ");
+                Serial.println(mqtt_username);
+                Serial.print("MQTT Password: ");
+                Serial.println(mqtt_password);
+                Serial.print("MQTT Topic: ");
+                Serial.println(topic);
+                
+            } else {
+                Serial.print("Failed with state ");
+                Serial.print(client.state());
+                delay(2000);
+            }
+        }
+    }
+
+    client.loop();// 周期性处理 MQTT 客户端的网络通信和消息维护，如果有消息则会调用一次回调函数
+    delay(100); // Delay for a short period in each loop iteration
+}
+
+
+//The parameters of the callback function are passed in automatically 
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    
+    // Notice the message is received by maid
+    Serial.print("Message arrived\n");     
+
+    // Start Receive the message 
+    String message;                  //message用于所订阅接收目标主题的状态消息
+    for (int i = 0; i < length; i++) 
+    {
+        message += (char) payload[i]; // Convert *byte to string
+    }
+    Serial.println("Message is"+message);  
+
+    // Analysis the received message
+    StaticJsonDocument<200> jsonDoc;   // 创建200字节大小的jsonDoc对象，用于存储解析后的JSON数据
+    deserializeJson(jsonDoc, message); // 解析这个message字符串，并将结果存储在 jsonDoc
+
+    // Judge if jsonDoc has the correct format data like standard json data,then process the mission  
+    // used json data content sample
+    //
+    // {
+    //  "maidcode":"01",
+    //  "status":"on",
+    //  "angle":0
+    // }
+
+    if (jsonDoc.is<JsonObject>()) 
+    {
+        JsonObject jsonObj = jsonDoc.as<JsonObject>();
+        const char* recieved_maid_code = jsonObj["maidcode"];
+        const char* recieved_maid_status=jsonObj["status"];
+        const char* recieved_maid_rotate_angle=jsonObj["angle"];
+
+
+        if (strcmp(recieved_maid_code, maidcode.c_str()) == 0 &&strcmp(recieved_maid_status, "on")==0 ) // Notice：strcmp匹配完全相同才返回0
+        {
+           Serial.print("on");
+           for(int dutyCycle = 0; dutyCycle < 1023; dutyCycle++)
+          { 
+            analogWrite(PWM_Pin, 200);
+            delay(1);
+          }
+        } 
+        else if (strcmp(recieved_maid_code, maidcode.c_str()) == 0 && strcmp(recieved_maid_status, "off")==0) 
+        {
+           Serial.print("off");
+           for(int dutyCycle = 0; dutyCycle < 1023; dutyCycle++)
+           { 
+            analogWrite(PWM_Pin, 128);
+            delay(1);
+           }
+        }
+    }
+}
+
+
+
+
+
+
+
